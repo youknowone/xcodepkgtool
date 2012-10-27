c165b996e473b36fe462e57b04d85d2ec24652eb
Jeong YunWon <jeong@youknowone.org>
2012-10-27 20:28:58 +0900

    fix test install

--- testproduct.sh
+++ testproduct.sh
@@ -5,4 +5,4 @@ cd ..
 
 version=`git describe --tags`
 version=${version#$tagprefix}
-installer -pkg "../build/Release/$appname $version.pkg" -target '/'
+installer -pkg "build/Release/$appname $version.pkg" -target '/'
